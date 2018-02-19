3eb16d3944a7bef33f07dbf63afa947f66723fe8
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-02-19 13:33:16 +0100

    edit travis ci and add es lint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  env: { es6: true, node: true },
+  rules: {
+    'no-console': 0,
+    'prefer-arrow-callback': 2,
+    'prefer-const': 2,
+    'no-var': 2,
+    'no-const-assign': 2,
+    eqeqeq: 2
+  }
+}
